bc5d02f75b778468921a7fbe38c27b032e9527a2
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-08-20 17:22:50 -0700

    validate password length

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # password-generator
+Javascript application to generate a random, secure password.
+## Project Features
+Generate button
+* Prompts user to select from the following criteria:
+    * Length between 8-128 characters
+    * Special characters
+    * Numbers
+    * 
+
--- script.js
+++ script.js
@@ -3,23 +3,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength;
-var confirmLower;
-var confirmUpper;
-var confirmNumber;
-var confirmSpecial;
-var userChoices;
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "=","@", "_", "~"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// convert to Uppercase
-var blankUpper = [];
-var toUpper = function (x) {
-  return x.toUpperCase();
-};
-upperCase = lowerCase.map(toUpper);
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@.#$%&*/+=_";
+var userChoices = "";
 
 
 // Write password to the #password input
@@ -28,95 +17,53 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Start Function
-function generatePassword() {
-  passwordLength = prompt("How many characters? Choose a number between 8 and 128.");
-  
-  if(!passwordLength) {
-    alert("Please choose a number betweeb 8 and 128.");
+function generatePassword(){
+  var result = "";
 
-  // validate user input
-  } else if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("You must choose a numner between 8 and 128");
-  
-  } else { 
-    confirmLower = confirm("Include lower case letters?");
-    confirmUpper = confirm("Include upper case letters?");
-    confirmNumber = confirm("Include numbers?");
-    confirmSpecial = confirm("Include special characters?");
-  };
+  var length = prompt("How many characters? Choose a number between 8 and 128.");
+  if(isNaN(length)){
+    alert("You must enter a number!");
+    return generatePassword()
+  }
+  if(length<8 || length> 128){
+    alert("Please choose a number between 8 - 128!");
+    return generatePassword()
+  }
+  // user criteria prompt
+  var hasUpper = confirm("Include upper case letters?");
+  var hasLower = confirm("Include lower case letters?");
+  var hasNumbers = confirm("Include numbers?");
+  var hasSpecial = confirm("Include special characters?");
 
-  // If no answer then
-  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
-    userChoices = alert("Please choose criteria.");
-  // 4 true options
-  } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    userChoices = lowerCase.concat(upperCase, numbers, special); 
-  }
-  // 3 true options
-  else if (confirmLower && confirmUpper && confirmNumber) {
-    userChoices = lowerCase.concat(upperCase, numbers);
-  }
-  else if (confirmLower && confirmUpper && confirmSpecial) {
-    userChoices = lowerCase.concat(upperCase, special);
-  }
-  else if (confirmLower && confirmNumber && confirmSpecial) {
-    userChoices = lowerCase.concat(numbers, special);
-  }
-  else if (confirmUpper && confirmNumber && confirmSpecial) {
-    userChoices = upperCase.concat(numbers, special);
-  }
-  // 2 true options
-  else if (confirmLower && confirmUpper) {
-    userChoices = lowerCase.concat(upperCase);
-  }
-  else if (confirmLower && confirmNumber) {
-    userChoices = lowerCase.concat(numbers);
-  }
-  else if (confirmLower && confirmSpecial) {
-    userChoices = lowerCase.concat(special);
-  }
-  else if (confirmUpper && confirmNumber) {
-    userChoices = upperCase.concat(numbers);
-  }
-  else if (confirmUpper && confirmSpecial) {
-    userChoices = upperCase.concat(special);
-  }
-  else if (confirmNumber && confirmSpecial) {
-    userChoices = numbers.concat(special);
-  }
-  // 1 true option
-  else if (confirmLower) {
-    userChoices = lowerCase;
-  }
-  else if (confirmUpper) {
-    userChoices = blankUpper.concat(upperCase);
-  }
-  else if (confirmNumber) {
-    userChoices = numbers;
-  }
-  else if (confirmSpecial) {
-    userChoices = special;
-  };
-
-  // Empty variable for the password length
-  var passwordBlank = [];
-  
-  // Loop for random selection
-  for (var i = 0; i < passwordLength; i++) {
-    var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
-    passwordBlank.push(allChoices);
+  if(!hasUpper&&!hasLower&&!hasNumbers&&!hasSpecial){
+    alert("You must choose a character type!");
+    return generatePassword()
   }
 
-  // Join and return the password 
-  var password = passwordBlank.join("");
-  return password; 
+  if(hasUpper){
+    userChoices += upper
+  }
+  if(hasLower){
+    userChoices += lower
+  }
+
+  if(hasNumbers) {
+    userChoices += numbers
+  }
+
+  if(hasSpecial) {
+    userChoices += special
+  }
+
+for (var i = 0; i < length; i++) {
+  result += userChoices.charAt(Math.floor(Math.random() * userChoices.length));
 }
+return result;
 
-
+}
